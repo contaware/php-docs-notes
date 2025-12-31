b038965ef1f49a7b9f33aa1e01f736036b7f113b
Oliver Pfister <oliver.pfister@contaware.com>
2025-12-31 08:56:24 +0100

    Make it run on CLI

--- html/submit.php
+++ html/submit.php
@@ -7,7 +7,8 @@
 </head>
 <body>
 <?php
-echo '$_SERVER[\'REQUEST_URI\']: ' . $_SERVER['REQUEST_URI'] . "<br>\n";
+if (isset($_SERVER['REQUEST_URI']))
+    echo '$_SERVER[\'REQUEST_URI\']: ' . $_SERVER['REQUEST_URI'] . "<br>\n";
 if (isset($_SERVER['PATH_INFO']))
     echo '$_SERVER[\'PATH_INFO\']: ' . $_SERVER['PATH_INFO'] . "<br>\n";
 if (isset($_SERVER['QUERY_STRING']))
@@ -15,16 +16,19 @@ if (isset($_SERVER['QUERY_STRING']))
 echo '$_SERVER[\'SCRIPT_NAME\']: ' . $_SERVER['SCRIPT_NAME'] . "<br>\n";
 echo '$_SERVER[\'PHP_SELF\']: ' . $_SERVER['PHP_SELF'] . "<br>\n";
 echo "<hr>\n";
-echo '$_SERVER[\'SERVER_PROTOCOL\']: ' . $_SERVER['SERVER_PROTOCOL'] . "<br>\n";
-echo '$_SERVER[\'REQUEST_METHOD\']: ' . $_SERVER['REQUEST_METHOD'] . "<br>\n";
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    foreach($_GET as $key=>$val)
-        echo htmlspecialchars("$key=$val", ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . "<br>\n";
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    foreach($_POST as $key=>$val)
-        echo htmlspecialchars("$key=$val", ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . "<br>\n";
+if (isset($_SERVER['SERVER_PROTOCOL']))
+    echo '$_SERVER[\'SERVER_PROTOCOL\']: ' . $_SERVER['SERVER_PROTOCOL'] . "<br>\n";
+if (isset($_SERVER['REQUEST_METHOD'])) {
+    echo '$_SERVER[\'REQUEST_METHOD\']: ' . $_SERVER['REQUEST_METHOD'] . "<br>\n";
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        foreach($_GET as $key=>$val)
+            echo htmlspecialchars("$key=$val", ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . "<br>\n";
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        foreach($_POST as $key=>$val)
+            echo htmlspecialchars("$key=$val", ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . "<br>\n";
+    }
+    echo "<hr>\n";
 }
-echo "<hr>\n";
 echo '$_SERVER[\'DOCUMENT_ROOT\']: ' . $_SERVER['DOCUMENT_ROOT'] . "<br>\n";
 echo '$_SERVER[\'SCRIPT_FILENAME\']: ' . $_SERVER['SCRIPT_FILENAME'] . "<br>\n";
 echo '__FILE__: ' . __FILE__ . "<br>\n";
